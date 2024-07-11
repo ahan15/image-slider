ce5f16d8e05d854b4d6d397b86e650503341f6c8
Abraham Han <seunghan77@gmail.com>
2024-07-11 15:17:30 -0500

    implemented btn event handlers

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 import './style.css'
 
-// fetch images
+const slide = document.querySelector('.slide');
+const root = document.querySelector(':root');
+let slideIndex = 1;
 
+function processImages(item) {
+  return `<img src="${item.url}" alt="${item.alt}">`;
+}
+
+function moveSlides() {
+  slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+  const slidesArray = [...slide.querySelectorAll('img')];
+  root.style.setProperty('--slide-progress', `${(100 / (slidesArray.length - 3)) * (slideIndex - 1)}%`);
+}
+
+// move when clicked
+function moveHandler(direction) {
+  slide.style.transition = `transform 450ms ease-in-out`;
+  direction !== 'right' ? (slideIndex -= 1) : (slideIndex += 1);
+  moveSlides();
+}
+
+
+// fetch images
 async function fetchImages() {
   await fetch('images.json')
     .then((response) => {
@@ -11,9 +32,25 @@ async function fetchImages() {
       return response.json();
     })
     .then((imageArray) => {
-      imageArray.push(imageArray[0]); // add first image to end
+      // clone first and last image
+      imageArray.push(imageArray[0]); 
       imageArray.unshift(imageArray[imageArray.length - 2]);
-      console.log(imageArray);
-    });
+      // show slider
+      slide.innerHTML = imageArray.map(processImages).join('');
+      moveSlides();
+    })
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
+    })
 }
 fetchImages();
+
+// click right btn
+document.querySelector('.slider__btn--right').addEventListener('click', () => {
+  moveHandler('right');
+});
+
+// click left btn
+document.querySelector('.slider__btn--left').addEventListener('click', () => {
+  moveHandler();
+});
