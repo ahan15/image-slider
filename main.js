c7874c036e5d98265727c69dd927e1c37739f13c
Abraham Han <seunghan77@gmail.com>
2024-07-11 13:26:48 -0500

    fetchImages() implemented

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+// fetch images
+
+async function fetchImages() {
+  await fetch('images.json')
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error('Network response was not okay');
+      }
+      return response.json();
+    })
+    .then((imageArray) => {
+      imageArray.push(imageArray[0]); // add first image to end
+      imageArray.unshift(imageArray[imageArray.length - 2]);
+      console.log(imageArray);
+    });
+}
+fetchImages();
